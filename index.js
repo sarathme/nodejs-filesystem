3b29e579f850c6afc6c2f39b5581aabc7a03b5f4
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-02 20:40:49 +0530

    <Patch> Change status code for redirect endpoint

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const app = express();
 fs.mkdir(path.join(__dirname, "results"), { recursive: true }, (err) => {
   // Error handling for directory creation
   if (err) {
-    res
-      .status(500)
-      .json({ status: "failed", message: "'Couldn't create a directory" });
+    console.log("Error creating directory");
   }
 
   // Logging to the console as the directory is created.
@@ -22,15 +20,14 @@ fs.mkdir(path.join(__dirname, "results"), { recursive: true }, (err) => {
   console.log("Directory created successfully");
 });
 
+// Create a API endpoint to redirect to /read-files route when / route is requested.
 app.get("/", (req, res) => {
-  res.redirect("/read-files");
+  res.status(302).redirect("/read-files");
 });
 
 // Create a API endpoint to create a file in results directory
 
 app.get("/create-file", (req, res) => {
-  // Using fs module to create a directory called results
-
   // defining variables for file name using Date class.
   const date = new Date().toISOString();
   const fileName = date.replace(/:/g, "-").replace("T", "_").split(".")[0];
@@ -83,20 +80,39 @@ app.get("/read-files", (req, res) => {
   });
 });
 
+// Creating an endpoint to remove all files from the results directory.
+
 app.get("/remove-files", (req, res) => {
+  // Defining the directory path (results) using path join method.
+
   const dirPath = path.join(__dirname, "results");
 
-  fs.readdir(dirPath, (err, data) => {
-    data.forEach((file) => {
-      const filePath = path.join(dirPath, file);
-      fs.unlinkSync(filePath);
-    });
+  // Reading all the files avaliable in the directory (results) using readdir method of fs module.
 
-    res.status(200).json({
-      status: "success",
-      message: "All files deleted successfully",
-      deletedFiles: data,
-    });
+  fs.readdir(dirPath, (err, data) => {
+    // Looping through the received data (filenames) using forEach method.
+
+    try {
+      data.forEach((file) => {
+        // Defining the file path of each object
+        const filePath = path.join(dirPath, file);
+
+        // Removing the file using unlinkSync (synchronous delete) method of the fs module.
+
+        fs.unlinkSync(filePath);
+      });
+
+      res.status(200).json({
+        status: "success",
+        message: "All files deleted successfully",
+        deletedFiles: data,
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: "failed",
+        message: "Unable to delete files",
+      });
+    }
   });
 });
 
