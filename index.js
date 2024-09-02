e2dd2bfcf3a74a7863d863540bf4c2006cad4766
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-02 16:22:34 +0530

    <Patch> Add port as per render website

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ app.get("/read-files", (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("App running on port 3000");
+  console.log(`App running on port ${port}`);
 });
